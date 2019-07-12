9b60e893e481008bdd93d9382f75e730c8a35322
erozonachi <erozonachi.james@gmail.com>
2019-07-12 09:40:21 +0100

    Add insert-project-action() to projects-controller

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,4 +1,5 @@
 const Projects = require('../data/helpers/projectModel');
+const Actions = require('../data/helpers/actionModel');
 
 const projectsController = {
   insertProject: async (req, res) => {
@@ -12,6 +13,22 @@ const projectsController = {
     } catch(error) {
       res.status(500).json({
         error: 'project could not be save at this time'
+      });
+    }
+  },
+
+  insertProjectAction: async (req, res) => {
+    try {
+      const action = await Actions.insert({ 
+        ...req.body, 
+        project_id: req.project.id, 
+        completed: false 
+      });
+
+      res.status(201).json(action);
+    } catch(error) {
+      res.status(500).json({
+        error: 'action could not be save at this time'
       });
     }
   },
