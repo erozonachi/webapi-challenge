fd6f0d1457e969bfea5e71d948ebde64a7ae1cc2
erozonachi <erozonachi.james@gmail.com>
2019-07-12 11:28:52 +0100

    Import and apply use-state hook in app component

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ store, setStore ] = useState({ projects: [] });
   return (
     <div className="App">
       <header className="App-header">
