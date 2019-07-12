a52a1555722380b96e3da7495b81aa281769bc32
erozonachi <erozonachi.james@gmail.com>
2019-07-12 10:02:59 +0100

    Add update-action() to the actions-controller

--- controllers/actionsController.js
+++ controllers/actionsController.js
@@ -1,7 +1,18 @@
 const Actions = require('../data/helpers/actionModel');
 
 const actionsController = {
+  updateAction: async (req, res) => {
+    try {
+      await Actions.update(req.action.id, req.body);
+      const action = await Actions.get(req.action.id);
 
+      res.status(200).json(action);
+    } catch(error) {
+      res.status(500).json({
+        error: 'action could not be modify at this time'
+      });
+    }
+  },
 }
 
 module.exports = actionsController;
