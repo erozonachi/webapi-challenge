b96c23302880394b971a552ea93ce6ba828dc9d6
erozonachi <erozonachi.james@gmail.com>
2019-07-12 11:39:25 +0100

    Import and apply use-effect hook in app component

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -17,6 +17,10 @@ function App() {
       console.log(error);
     });
   }
+
+  useEffect(() => {
+    fetchProjects()
+  }, [ store ]);
   return (
     <div className="App">
       <header className="App-header">
