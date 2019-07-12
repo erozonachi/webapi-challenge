5319d35364f7544880e114717ae57fab55871bd7
erozonachi <erozonachi.james@gmail.com>
2019-07-12 09:32:39 +0100

    Add insert-project function to projects-controller

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,7 +1,20 @@
 const Projects = require('../data/helpers/projectModel');
 
 const projectsController = {
+  insertProject: async (req, res) => {
+    try {
+      const project = await Projects.insert({ 
+        ...req.body, 
+        completed: false 
+      });
 
+      res.status(201).json(project);
+    } catch(error) {
+      res.status(500).json({
+        error: 'project could not be save at this time'
+      });
+    }
+  },
 };
 
 module.exports = projectsController;
